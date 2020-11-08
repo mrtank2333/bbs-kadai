fa87588b43a89c55fbda03b3145227eab9a9a40e
mrtank2333 <46072607+mrtank2333@users.noreply.github.com>
2020-11-08 18:45:55 +0900

    Update register_process.php

--- users/register_process.php
+++ users/register_process.php
@@ -1,6 +1,4 @@
 <?php
-// 必須項目のログインIDとパスワードのどちらでも欠けていたら
-// 何も処理せず閲覧画面に戻す (リダイレクト)
 if (empty($_POST['login_id']) || empty($_POST['password'])) {
   header("HTTP/1.1 302 Found");
   header("Location: ./register.php");
@@ -12,7 +10,6 @@ require_once '../script/db_func.php';
 $db_func = new db_func();
 $dbh = $db_func -> acc_db();
 // 会員テーブル users に1行insert
-// SQLインジェクションを防ぐためにプレースホルダを使う
 $insert_sth = $dbh->prepare("INSERT INTO users (login_id, password) VALUES (:login_id, :password)");
 $insert_sth->execute([
     ':login_id' => $_POST['login_id'],
